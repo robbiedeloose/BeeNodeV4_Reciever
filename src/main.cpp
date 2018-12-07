ac41014d28d3fe56d47e5b092c6e224cd8d5ed2e
robbiedeloose <robbiedeloose@users.noreply.github.com>
2018-12-07 15:46:06 +0100

    Got the timings right replying to i2c request from co
    not yet sending real data

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-
+#include <Wire.h>
 ///////////////////////////////////// RADIO ////////////////////////////////////
 #include <RF24.h>
 #include <RF24Network.h>
@@ -19,22 +19,51 @@ struct Payload_t {
 };
 
 ////////////////////////// FUNCTION DECLARATIONS ///////////////////////////////
-// radio functions
 void initRFRadio(uint8_t channel, uint16_t nodeAddress);
 void checkForNetworkData();
+void requestEvent();
+
+uint8_t isAwake = 0;
+
+void wake() {
+  detachInterrupt(1); 
+  isAwake = 1;
+}
 
 /////////////// SETUP //////////////////////////////////////////////////////////
 void setup() {
-  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Wire.begin();   
+  Serial.begin(115200);
   delay(1000);
   Serial.println(F("BeeNode v4.0.1 - nRF24 tester"));
   initRFRadio(90, thisNode); // start nRF24l radio
 
+  pinMode(3,INPUT); 
+
+  Wire.begin(8);  // join i2c bus with address #8
+  Wire.onRequest(requestEvent); // register event
+      attachInterrupt(digitalPinToInterrupt(3), wake, FALLING);
   Serial.println("init complete");
+  delay(1000);
 }
 
+
+
 /////////////// LOOP ///////////////////////////////////////////////////////////
-void loop() { checkForNetworkData(); }
+void loop() { 
+
+  if (isAwake == 1) {
+    Serial.println("awake");
+    delay(1000);
+  }
+  else {
+    Serial.println("sleep");
+    checkForNetworkData();
+    delay(250);
+    network.sleepNode(1, 0); // 8 cycles of 4 seconds
+  }
+}
 
 //// Getting data //////////////////////////////////////////////////////////////
 void checkForNetworkData() {
@@ -48,8 +77,6 @@ void checkForNetworkData() {
     for (byte b : payload.id)
       Serial.print(b, HEX);
     Serial.println();
-    Serial.print(" Node ID: ");
-    Serial.println(payload.temp);
   }
 }
 
@@ -62,6 +89,15 @@ void initRFRadio(uint8_t channel, uint16_t nodeAddress) { // clean
 
   SPI.begin();
   radio.begin();
-  // radio.setPALevel(HIGH);
+  radio.setPALevel(HIGH);
   network.begin(channel, nodeAddress);
+  network.setup_watchdog(8);
+}
+
+void requestEvent() {
+  isAwake = 0;
+  Serial.println("trigger");
+  Wire.write("hello "); // respond with message of 6 bytes as expected by master
+  Serial.println("set interrupt");
+  attachInterrupt(digitalPinToInterrupt(3), wake, LOW);
 }
