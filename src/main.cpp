0d16f16a70eb1135ac766998a9ec02bf721419eb
robbiedeloose <robbiedeloose@users.noreply.github.com>
2018-12-08 22:19:36 +0100

    sending whole buffer to co
    co is requesting data 6 times and recieves entire payload

--- src/main.cpp
+++ src/main.cpp
@@ -26,21 +26,24 @@ Payload_t plBuffer[BUFFERSIZE];
 void initRFRadio(uint8_t channel, uint16_t nodeAddress);
 void checkForNetworkData();
 void fillBufferArray(Payload_t *payloadAddress);
+void clearBufferArray();
+void displayBufferArray();
 void requestEvent();
 void wake();
 
 uint8_t isAwake = 0;
+uint8_t readNumber = 0;
 
 /////////////// SETUP //////////////////////////////////////////////////////////
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
-  Wire.begin();   
+  Wire.begin();
   Serial.begin(115200);
   delay(1000);
-  Serial.println(F("BeeNode v4.0.1 - nRF24 tester"));
+  Serial.println(F("BeeNode v4.0.1 - nRF24 reciever"));
   initRFRadio(90, thisNode); // start nRF24l radio
-  pinMode(3,INPUT); 
-  Wire.begin(8);  // join i2c bus with address #8
+  pinMode(3,INPUT);
+  Wire.begin(8); // join i2c bus with address #8
   Wire.onRequest(requestEvent); // register event
       attachInterrupt(digitalPinToInterrupt(3), wake, FALLING);
   Serial.println("init complete");
@@ -57,7 +60,7 @@ void loop() {
     Serial.println("sleep");
     checkForNetworkData();
     delay(250);
-    network.sleepNode(1, 0); // 8 cycles of 4 seconds
+    network.sleepNode(1, digitalPinToInterrupt(2)); // x cycles of watchdog time (see below), interupt pin
   }
 }
 
@@ -71,7 +74,10 @@ void checkForNetworkData() {
  
     // fill buffer
     fillBufferArray(&payload);
-
+    Serial.print(" Node: ");
+    for (int i = 0; i < 4; i++) {
+      Serial.print(payload.id[i]);
+    }
     Serial.println();
     Serial.print(" - temp: ");
     Serial.println(payload.temp);
@@ -91,7 +97,7 @@ void fillBufferArray(Payload_t *payloadAddress) {
     if (plBuffer[bufferLocation].bat != 0)
       bufferLocation++;
   }
-  Serial.print(F(" Array position "));
+  Serial.print(F("Array position "));
   Serial.println(bufferLocation); // print the buffer location that is used
   for (int i = 0; i < 4; i++) {
     plBuffer[bufferLocation].id[i] = payloadAddress->id[i];
@@ -100,6 +106,31 @@ void fillBufferArray(Payload_t *payloadAddress) {
   plBuffer[bufferLocation].humidity = payloadAddress->humidity;
   plBuffer[bufferLocation].bat = payloadAddress->bat;
   plBuffer[bufferLocation].weight = payloadAddress->weight;
+}
+
+void clearBufferArray(){
+  Serial.println("Clear buffer array");
+  for (int i = 0; i < BUFFERSIZE; i++) {
+    plBuffer[i].bat = 0;
+  }
+}
+
+void displayBufferArray() {
+  for (int bufferLocation = 0; bufferLocation < BUFFERSIZE; bufferLocation++) {
+    Serial.print(" Node: ");
+    for (int i = 0; i < 4; i++) {
+      Serial.print(plBuffer[bufferLocation].id[i]);
+    }
+    Serial.println();
+    Serial.print(" - temp: ");
+    Serial.println(plBuffer[bufferLocation].temp);
+    Serial.print(" - hum: ");
+    Serial.println(plBuffer[bufferLocation].humidity);
+    Serial.print(" - bat: ");
+    Serial.println(plBuffer[bufferLocation].bat);
+    Serial.print(" - weight: ");
+    Serial.println(plBuffer[bufferLocation].weight);
+  }
 }
 
 //////////// init nrf radio ////////////////////////////////////////////////////
@@ -117,14 +148,20 @@ void initRFRadio(uint8_t channel, uint16_t nodeAddress) { // clean
 }
 
 void requestEvent() {
-  isAwake = 0;
-  Serial.println("trigger");
+  Serial.print("read ");
+  Serial.println(readNumber);
   char buf[120] = "";
-  sprintf(buf, "%i,%u,%u,%02X%02X%02X%02X", plBuffer[0].temp,plBuffer[0].humidity, plBuffer[0].bat, plBuffer[0].id[0], plBuffer[0].id[1], plBuffer[0].id[2], plBuffer[0].id[3]);
+  sprintf(buf, "%02X%02X%02X%02X,%04i,%03u,%03u,%06u", plBuffer[readNumber].id[0], plBuffer[readNumber].id[1], plBuffer[readNumber].id[2], plBuffer[readNumber].id[3], plBuffer[readNumber].temp, plBuffer[readNumber].humidity, plBuffer[readNumber].bat, plBuffer[readNumber].weight);
   Serial.println(buf);
   Wire.write(buf); // respond with message of 6 bytes as expected by master
-  Serial.println("set interrupt");
-  attachInterrupt(digitalPinToInterrupt(3), wake, LOW);
+  readNumber ++;
+  if (readNumber > 5) { //which means all payloads are sent
+    isAwake = 0;
+    readNumber = 0;
+    clearBufferArray();
+    Serial.println("set interrupt");
+    attachInterrupt(digitalPinToInterrupt(3), wake, LOW);
+  }
 }
 
 void wake() {
